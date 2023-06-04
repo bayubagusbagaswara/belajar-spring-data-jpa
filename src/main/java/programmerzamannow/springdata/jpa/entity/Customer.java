20b4a2cc126249cc71fd18108f4ac9c31d3375d0
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-06-04 21:35:40 +0700

    add entity Customer

---
+++ src/main/java/programmerzamannow/springdata/jpa/entity/Customer.java
@@ -0,0 +1,43 @@
+package programmerzamannow.springdata.jpa.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    private String id;
+
+    private String name;
+
+    @Column(name = "primary_email")
+    private String primaryEmail;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPrimaryEmail() {
+        return primaryEmail;
+    }
+
+    public void setPrimaryEmail(String primaryEmail) {
+        this.primaryEmail = primaryEmail;
+    }
+}
