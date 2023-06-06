477cb2f2680f22909c0de8a0cf59ac4d3fb5cd09
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-06-06 20:58:02 +0700

    add test CategoryRepositoryTest

---
+++ src/test/java/programmerzamannow/springdata/jpa/repository/CategoryRepositoryTest.java
@@ -0,0 +1,25 @@
+package programmerzamannow.springdata.jpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import programmerzamannow.springdata.jpa.entity.Category;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class CategoryRepositoryTest {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Test
+    void insert() {
+        Category category = new Category();
+        category.setName("GADGET");
+
+        categoryRepository.save(category);
+
+        assertNotNull(category.getId());
+    }
+}
