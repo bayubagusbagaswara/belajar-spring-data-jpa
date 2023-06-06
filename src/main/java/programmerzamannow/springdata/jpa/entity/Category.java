0e4dc433c320b813d80260c7ae2d935d60b38d5b
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-06-06 20:46:15 +0700

    update entity Category

--- src/main/java/programmerzamannow/springdata/jpa/entity/Category.java
+++ src/main/java/programmerzamannow/springdata/jpa/entity/Category.java
@@ -1,40 +1,23 @@
 package programmerzamannow.springdata.jpa.entity;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Entity
 @Table(name = "categories")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Category {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private String id;
+    private Long id;
 
     private String name;
 
-    private String description;
-
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getDescription() {
-        return description;
-    }
-
-    public void setDescription(String description) {
-        this.description = description;
-    }
 }
