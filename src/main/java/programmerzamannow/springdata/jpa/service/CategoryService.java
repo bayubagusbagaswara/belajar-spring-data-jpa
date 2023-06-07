619ee09c15b64f624b46c3599860e0f8bb05599c
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-06-07 15:45:50 +0700

    transaction manager

--- src/main/java/programmerzamannow/springdata/jpa/service/CategoryService.java
+++ src/main/java/programmerzamannow/springdata/jpa/service/CategoryService.java
@@ -2,8 +2,12 @@ package programmerzamannow.springdata.jpa.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.TransactionDefinition;
+import org.springframework.transaction.TransactionStatus;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.transaction.support.DefaultTransactionDefinition;
 import org.springframework.transaction.support.TransactionOperations;
 import programmerzamannow.springdata.jpa.entity.Category;
 import programmerzamannow.springdata.jpa.repository.CategoryRepository;
@@ -16,6 +20,31 @@ public class CategoryService {
 
     @Autowired
     private TransactionOperations transactionOperations;
+
+    @Autowired
+    private PlatformTransactionManager platformTransactionManager;
+
+    public void manual() {
+        DefaultTransactionDefinition definition = new DefaultTransactionDefinition();
+        definition.setTimeout(10); // jika lewat 10 detik, maka error
+        definition.setPropagationBehavior(TransactionDefinition.PROPAGATION_REQUIRED);
+
+        TransactionStatus transaction = platformTransactionManager.getTransaction(definition);
+
+        try {
+            for (int i = 0; i < 5; i++) {
+                Category category = new Category();
+                category.setName("Category " + i);
+                categoryRepository.save(category);
+            }
+            error();
+            platformTransactionManager.commit(transaction);
+        } catch (Throwable throwable) {
+            // ketika ketemu exception, maka rollback
+            platformTransactionManager.rollback(transaction);
+            throw throwable;
+        }
+    }
 
     public void error() {
         throw new RuntimeException("Ups");
