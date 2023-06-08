470d014c41ed9b12dd4496f28e7a47fca5808259
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-06-08 11:28:27 +0700

    add query method sorting findAllByCategory_Name

--- src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
+++ src/main/java/programmerzamannow/springdata/jpa/repository/ProductRepository.java
@@ -1,5 +1,6 @@
 package programmerzamannow.springdata.jpa.repository;
 
+import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import programmerzamannow.springdata.jpa.entity.Product;
@@ -10,4 +11,6 @@ import java.util.List;
 public interface ProductRepository extends JpaRepository<Product, Long> {
 
     List<Product> findAllByCategory_Name(String name);
+
+    List<Product> findAllByCategory_Name(String name, Sort sort);
 }
