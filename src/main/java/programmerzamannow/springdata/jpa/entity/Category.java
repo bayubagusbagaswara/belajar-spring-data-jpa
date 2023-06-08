085dbaf41401fd20d86f60ea8164a622c6bcc05e
bayubagusbagaswara <bagaszwara12@gmail.com>
2023-06-08 11:06:50 +0700

    add relation with Product

--- src/main/java/programmerzamannow/springdata/jpa/entity/Category.java
+++ src/main/java/programmerzamannow/springdata/jpa/entity/Category.java
@@ -6,6 +6,8 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.List;
+
 @Entity
 @Table(name = "categories")
 @Getter
@@ -20,5 +22,8 @@ public class Category {
 
     @Column(name = "name")
     private String name;
+
+    @OneToMany(mappedBy = "category")
+    private List<Product> products;
 
 }
